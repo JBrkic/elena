b16c173310485c35e5ee1c78ea7d03ef995dd680
JBrkic <josip.brkic08@gmail.com>
2019-05-27 20:51:02 +0200

    hi

---
+++ assets/js/functions.js
@@ -0,0 +1,32 @@
+$('#toggle').click(function () {
+  $(this).toggleClass('active');
+  $('#overlay').toggleClass('open');
+});
+
+$(function () {
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    $('#overlay').toggleClass('open');
+    $('#toggle').toggleClass('active');
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        target.focus(); // Setting focus
+        if (target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+          target.focus(); // Setting focus
+        };
+        return false;
+      }
+    }
+  });
+});
+
+$(window).on('load', function () {
+  $('body').addClass('loaded');
+});
